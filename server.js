187276f54ef61dda599e7d4d146ea98bb8f3bc35
Unicorn Barf <nolan.spence7@gmail.com>
2022-07-29 16:31:24 -0700

    Setup server, connection, and routes directory.

---
+++ server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const sequelize = require('./config/connection');
+const routes = require('./routes');
+
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({
+  helpers,
+});
+
+const sessionSettings = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3001;
+
+// Template Engine Setup
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// middlewares
+app.use(express.static('public'));
+app.use(session(sessionSettings));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Doing a death drop on PORT: ${PORT}`));
+});
